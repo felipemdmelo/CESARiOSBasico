1931f4c28be8510be7925bf7e4a2918b93705f66
Aluno <aluno@LABMAC20.local>
2018-05-11 21:01:04 -0300

    testando envio ao git

--- LifeCycle/LifeCycle/ViewController.swift
+++ LifeCycle/LifeCycle/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: VCLLoggingViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib teste.
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
